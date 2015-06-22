164db99252e1c84c01acb4e9c29b007e203dd1c1
eieshine <eieshine@naver.com>
2015-06-22 10:56:59 +0900

    update showthinginfo
    update showthinginfo

--- project/IoTMS/src/NodeManager/NodeManager.java
+++ project/IoTMS/src/NodeManager/NodeManager.java
@@ -99,7 +99,7 @@ public class NodeManager implements AdapterEventListener {
 		System.out.println ("[NM - Process] [EventBus] ShowThingInfo: " + info);
 		
 		JSONArray targets = new JSONArray();
-		targets.add("UI");
+		targets.add("RuleManager");
 		sendEvent(info, targets, "ThingMonitor");
 	}
 
