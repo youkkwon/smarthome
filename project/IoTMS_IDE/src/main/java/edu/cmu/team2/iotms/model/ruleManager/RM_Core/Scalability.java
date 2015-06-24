076d8b3a34fa6a05b2dcefbe31d23a9b0eac7c2e
bhjung <bohyun.jung@lge.com>
2015-06-25 08:29:56 +0900

    TestOnly -

---
+++ project/IoTMS_IDE/src/main/java/edu/cmu/team2/iotms/model/ruleManager/RM_Core/Scalability.java
@@ -0,0 +1,67 @@
+package edu.cmu.team2.iotms.model.ruleManager.RM_Core;
+
+import java.util.Calendar;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import edu.cmu.team2.iotms.model.eventBus.IoTMSEventBus;
+
+//Singleton
+public class Scalability extends Thread {
+
+	private String nodeID = "";
+	private static Scalability scheduler = new Scalability(); 
+
+	private Calendar calendar = Calendar.getInstance( ); 
+	
+	private Scalability()
+	{
+		
+	}
+	
+	public static Scalability getInstance()
+	{
+		return scheduler;		
+	}
+	
+	// TODO expire 처리. 
+	public void run()
+	{
+		int number = 0;
+		
+		while (true) 
+		{			
+			try {
+				sendEvent(number++);
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void execute(JSONObject JSONMsg)
+	{
+		System.out.println("Pong : " + JSONMsg.get("Value") + " Time : " + System.currentTimeMillis() / 1000);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void sendEvent(int number)
+	{
+		JSONObject 	JSONMsg = new JSONObject();
+		JSONArray 	targets = new JSONArray();
+		
+		targets.add("NodeManager");
+		JSONMsg.put("Targets", targets);
+		JSONMsg.put("Job", "Ping");
+		JSONMsg.put("NodeID",  nodeID);
+		JSONMsg.put("Value", Integer.toString(number));	
+		
+		System.out.println("Ping : " + number + " Time : " + System.currentTimeMillis() / 1000);
+		
+		// post event.
+		IoTMSEventBus.getInstance().postEvent(JSONMsg);
+	}
+}
