ef757226c9a81648a62efaf0a04d6c55f8019631
eieshine <eieshine@naver.com>
2015-06-26 11:47:58 +0900

    Revert "performance test"
    This reverts commit 335fe21c3609b1792fdaab3406b4f4885aae9c80.

--- project/IoTMS_IDE/src/main/java/edu/cmu/team2/iotms/model/eventBus/NM_EventBusReceiver.java
+++ project/IoTMS_IDE/src/main/java/edu/cmu/team2/iotms/model/eventBus/NM_EventBusReceiver.java
@@ -51,8 +51,6 @@ public class NM_EventBusReceiver extends Thread {
 			NodeManager.getInstance().registerNode(JSONMsg);
 		else if (job.equalsIgnoreCase("RemoveNode"))
 			NodeManager.getInstance().removeNode(JSONMsg);
-		else if (job.equalsIgnoreCase("Ping"))
-			NodeManager.getInstance().ping(JSONMsg);
 		else
 		{
 			System.out.println ("Not valid event, ignore it.");
